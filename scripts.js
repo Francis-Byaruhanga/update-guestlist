0eeb1f2763ecd88519b9d1440478b10b8a452a0a
francisb <byaruhangafrancis23@gmail.com>
2026-02-21 14:58:11 +0300

    Add conditional logic to admit people to either refused or admitted list

--- scripts.js
+++ scripts.js
@@ -19,6 +19,10 @@ refused.textContent = "Refused: ";
 for (let i = 0; i < people.length; i++) {
     const person = people[i];
     // Write the logic below
-    
+    if (person === "Phil" || person === "Lola") {
+        // Add to refused list
+    } else {
+        // Add to admitted list
+    }
 }
 
